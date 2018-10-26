4a804a66aa4eeccf3da62b2aea9f6ba07b7cbf16
Vince Jones <vjones@nerdery.com>
2018-10-26 10:24:07 -0500

    Production build.

---
+++ client/build/precache-manifest.2722ae1891283ae910626c437ce06cbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3cc39e9823257b1529d",
+    "url": "/static/js/main.e3cc39e9.chunk.js"
+  },
+  {
+    "revision": "2198003f29bab8e0e624",
+    "url": "/static/js/1.2198003f.chunk.js"
+  },
+  {
+    "revision": "e3cc39e9823257b1529d",
+    "url": "/static/css/main.b8d93851.chunk.css"
+  },
+  {
+    "revision": "c296630119f92dcc72f5efd956fb8cf1",
+    "url": "/index.html"
+  }
+];
