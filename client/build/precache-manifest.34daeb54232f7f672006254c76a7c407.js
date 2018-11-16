2099b280fb201719d854f2aba0806cc9ce11765b
Travis CI <travis@travis-ci.org>
2018-11-16 22:40:31 +0000

    Automated commit by Travis CI for Build 91726839

---
+++ client/build/precache-manifest.34daeb54232f7f672006254c76a7c407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58892137362e5574b4ef",
+    "url": "/static/css/main.4e8eac74.chunk.css"
+  },
+  {
+    "revision": "58892137362e5574b4ef",
+    "url": "/static/js/main.58892137.chunk.js"
+  },
+  {
+    "revision": "6b6d9c95163944bdf690",
+    "url": "/static/js/1.6b6d9c95.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac727edde61cbf3cc9ecd4b0ab93606d",
+    "url": "/index.html"
+  }
+];
