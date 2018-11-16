da1b6f7fec8c3d9e56ac3464e2a3776697e906d4
Vince Jones <vjones@nerdery.com>
2018-11-16 11:23:01 -0600

    Rename modal.css

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 
-import './_modal.css';
+import './modal.css';
 
 /**
  * DeleteModal component.
