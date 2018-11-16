c99a535c7bce3a3e230c6f03f85a65883fd6b3f1
Vince Jones <vjones@nerdery.com>
2018-11-16 14:52:02 -0600

    Style login page.

--- client/src/components/Page/Login/LoginPageContent.js
+++ client/src/components/Page/Login/LoginPageContent.js
@@ -7,15 +7,22 @@ import React from 'react';
  */
 const LoginPageContent = ({ githubId = '' }) => {
   return (
-    <div className="LoginPageContent-container">
-      <p>
-        If you wish to contribute recipes to the Recipe App please login with
-        Github and send your send your Github ID to{' '}
-        <a href="mailto:vincentjones4426@gmail.com">
-          vincentjones4426@gmail.com
-        </a>
-        . You will then be added to approved users.
-      </p>
+    <div className="login__container">
+      <h3>If you wish to contribute recipes</h3>
+      <ol>
+        <li>Login with Github using the button below</li>
+        <li>
+          You will be presented with your Github ID, send this ID to{' '}
+          <a href="mailto:vincentjones4426@gmail.com">
+            vincentjones4426@gmail.com
+          </a>
+          {' '} stating that you would like to have access to Add/Edit/Delete content on this site.
+        </li>
+        <li>
+          You will then be added to approved users and will be able to
+          Add/Edit/Delete content.
+        </li>
+      </ol>
 
       {githubId !== '' && (
         <div>
