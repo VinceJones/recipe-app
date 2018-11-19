0d4da45f4013d8b19334129e62c63f1dacc2ba99
Vince Jones <vjones@nerdery.com>
2018-11-19 14:30:05 -0600

    Redo login page css.

--- client/src/components/Page/Login/LoginPage.js
+++ client/src/components/Page/Login/LoginPage.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import Page from '../Page';
-import GitHubLogin from 'react-github-login';
 import LoginPageContent from './LoginPageContent';
-import AuthHandler from '../../AuthHandler';
 import userServiceSingleton from '../../UserService';
-import './LoginPage.css';
-
-const authHandler = new AuthHandler();
 
 export default class LoginPage extends Component {
   /**
@@ -35,16 +30,11 @@ export default class LoginPage extends Component {
   render() {
     return (
       <Page pageTitle="Login">
-        <LoginPageContent githubId={userServiceSingleton.githubId} />
-        <div>
-          <GitHubLogin
-            clientId={userServiceSingleton.clientId}
-            onSuccess={this.onSuccess}
-            onFailure={this.onFailure}
-            redirectUri={authHandler.endpoints.redirectUri}
-            className="btn btn_secondary btn_login"
-          />
-        </div>
+        <LoginPageContent
+          githubId={userServiceSingleton.githubId}
+          onSuccess={this.onSuccess}
+          onFailure={this.onFailure}
+        />
       </Page>
     );
   }
--- client/src/components/Page/Login/LoginPageContent.js
+++ client/src/components/Page/Login/LoginPageContent.js
@@ -1,34 +1,49 @@
 import React from 'react';
+import GitHubLogin from 'react-github-login';
+import AuthHandler from '../../AuthHandler';
+import userServiceSingleton from '../../UserService';
+
+const authHandler = new AuthHandler();
 
 /**
  * LoginPageContent component.
  *
  * @public
  */
-const LoginPageContent = ({ githubId = '' }) => {
+const LoginPageContent = ({ githubId = '00000000', onSuccess, onFailure }) => {
   return (
-    <div className="login__container">
-      <h3>If you wish to contribute recipes</h3>
-      <ol>
-        <li>Login with Github using the button below</li>
-        <li>
-          You will be presented with your Github ID, send this ID to{' '}
-          <a href="mailto:vincentjones4426@gmail.com">
-            vincentjones4426@gmail.com
-          </a>
-          {' '} stating that you would like to have access to Add/Edit/Delete content on this site.
-        </li>
-        <li>
-          You will then be added to approved users and will be able to
-          Add/Edit/Delete content.
-        </li>
-      </ol>
+    <div className="container__login">
+      <div className="header__login">
+        <h3>If you wish to contribute recipes</h3>
+      </div>
+      <div className="body__login">
+        <ol>
+          <li>Login with Github using the button below</li>
+          <li>
+            You will be presented with your Github ID, send this ID to{' '}
+            <a href="mailto:vincentjones4426@gmail.com">
+              vincentjones4426@gmail.com
+            </a>{' '}
+            stating that you would like to have access to Add/Edit/Delete
+            content on this site.
+          </li>
+          <li>
+            You will then be added to approved users and will be able to
+            Add/Edit/Delete content.
+          </li>
+        </ol>
+      </div>
 
-      {githubId !== '' && (
-        <div>
-          <h3>Github ID: {githubId}</h3>
-        </div>
-      )}
+      <div className="footer__login">
+        <h3>Github ID: {githubId}</h3>
+        <GitHubLogin
+          clientId={userServiceSingleton.clientId}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          redirectUri={authHandler.endpoints.redirectUri}
+          className="btn btn_secondary btn_login"
+        />
+      </div>
     </div>
   );
 };
